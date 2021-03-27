e1361f297ea3aa773b3154276ea17c930fa58c0c
gvinianidzegivi <gvinianidzegivi@gmail.com>
2021-03-27 18:58:02 +0400

    added custom scss style structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 function App() {
   return (
     <div className="container">
-      <button className="bg-red-400">Hey</button>
-      <h1>AE</h1>
+      <footer>
+        <div className="bg-purple-800 text-center">
+          <span className="bg-white text-purple-800 px-3">
+            &copy; Givi Gvinianidze
+          </span>
+          <span className="text-white px-3">
+            TailwindCSS, Bootstrap-grid, Custom SCSS styles structure for React
+          </span>
+        </div>
+      </footer>
     </div>
   );
 }
