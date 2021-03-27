2b5a1c3c8554082918f32f91210e52f11e4d0236
gvinianidzegivi <gvinianidzegivi@gmail.com>
2021-03-27 17:15:14 +0400

    added responsive font size system

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 function App() {
   return (
-    <div className="container-fluid">
+    <div className="container">
       <button className="bg-red-400">Hey</button>
+      <h1>AE</h1>
     </div>
   );
 }
