6a8c83944533ce687df2169832fc1d1b30d9d39a
gvinianidzegivi <gvinianidzegivi@gmail.com>
2021-03-27 17:09:07 +0400

    added bootstrap grid system

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import "./App.css";
-
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container-fluid">
+      <button className="bg-red-400">Hey</button>
+    </div>
+  );
 }
 
 export default App;
